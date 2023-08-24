e6f2be1f0b766ee9975c1d98ba7f90022502436b
Alexander Peregontsev <110561969+APeregontsev@users.noreply.github.com>
2023-08-24 05:37:43 +0300

    Add files via upload
    // Lets calculate start and end Dates for future periods, because past dates cost 30X in API-requests..

--- src/startData.ts
+++ src/startData.ts
@@ -2,7 +2,33 @@ import { SelectOptionType } from "./UI/Select/Select";
 import { TripType } from "./useTrips";
 
 // Starting data for TripsList
-export const startData: TripType = { id: 1, name: "Berlin", startDate: "2023-08-30", endDate: "2023-08-31" };
+
+// Lets calculate start and end Dates for future periods, because past dates cost 30X in API-requests..
+const startDate = new Date(new Date().setDate(new Date().getDate() + 1));
+const endDate = new Date(new Date().setDate(new Date().getDate() + 5));
+
+const startDateString =
+  startDate.getFullYear() +
+  "-" +
+  (startDate.getMonth() + 1).toString().padStart(2, "0") +
+  "-" +
+  startDate.getDate().toString().padStart(2, "0");
+
+const endDateString =
+  endDate.getFullYear() +
+  "-" +
+  (endDate.getMonth() + 1).toString().padStart(2, "0") +
+  "-" +
+  endDate.getDate().toString().padStart(2, "0");
+
+// Starting data for TripsList
+
+export const startData: TripType = {
+  id: 1,
+  name: "Berlin",
+  startDate: startDateString,
+  endDate: endDateString,
+};
 
 //Starting data for Select
 export const selectData: SelectOptionType[] = [
@@ -37,4 +63,5 @@ export const selectData: SelectOptionType[] = [
 ];
 
 // API key
+
 export const apiKey = "MESYLPREKWZD9EZSQMFGDNE2V";
